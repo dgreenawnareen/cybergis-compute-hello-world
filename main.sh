fa85e4f4ed083d62e02ecda85052113a9c821118
Alexander Michels <michels9@illinois.edu>
2024-09-09 11:11:19 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,3 +3,6 @@
 mkdir $result_folder/demo_quick_start_$SLURM_PROCID
 chmod 755 $result_folder/demo_quick_start_$SLURM_PROCID
 python main.py
+
+mkdir -p $result_folder/executable
+cp -r $executable_folder/* $result_folder/executable/
